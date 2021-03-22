d5aaccdee8eb074d82db4cda991654f92cdb0fbf
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-03-22 14:43:39 -0700

    cap and filter passing using array map

--- cap-and-filter.js
+++ cap-and-filter.js
@@ -1,13 +1,11 @@
 const copyAndFilter = (arr) => {
     const newArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        const element = arr[i];
+    arr.map(element => {
         const capEle = element.toUpperCase();
         if (capEle.startsWith('F')) {
             newArr.push();
         } else newArr.push(capEle);
-        // newArr.push(capEle);
-    }
+    });
     return newArr;
 };
 
