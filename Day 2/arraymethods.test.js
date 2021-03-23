7fadc32ef35af8fc864032be9958ad96c8874f9c
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-03-23 14:19:31 -0700

    tests for index of passing

--- Day 2/arraymethods.test.js
+++ Day 2/arraymethods.test.js
@@ -1,5 +1,6 @@
 const map = require('./map');
 const filter = require('./filter');
+const findIndex = require('./findIndex');
 
 describe('map function', () => {
     it('maps over array uses given callback and returns new array of same length', () => {
@@ -13,5 +14,14 @@ describe('filter function', () => {
     it('filters an array uses given callback and returns new array of truthy elements', () => {
         const newArr = filter([2, 3, 4], (n) => n % 2 === 0);
         expect(newArr).toEqual([2, 4]);
+    });
+});
+
+describe('find index function', () => {
+    it('returns index of first truthy element, or -1 if no truthy elements', () => {
+        const newArr = findIndex([2, 3, 4], (n) => n % 2 === 0);
+        const secondArr = findIndex([1, 3, 4, 5, 6], (n) => n % 2 === 0);
+        expect(newArr).toEqual(0);
+        expect(secondArr).toEqual(2);
     });
 });
---
+++ Day 2/findIndex.js
@@ -0,0 +1,10 @@
+const findIndex = (arr, callback) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i])) {
+            return i;
+        }
+    }
+    return -1;
+};
+
+module.exports = findIndex;
